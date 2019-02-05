95d431ad271766757a2c58fe5c3d21ab36bf5b9a
Oleksii Bunyk <oleksabunyk@gmail.com>
2019-02-05 11:32:34 +0200

    Added models for ingridients, recipe and freidge

---
+++ Picker/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length = 40, unique = True)
+    units = models.CharField(max_length = 10)
+    description = models.TextField(blank = True, default = '')
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    amount = models.DecimalField(max_digits = 10, decimal_places = 1, null = True)
+
+    class Meta:
+        unique_together = (('dish', 'ingredient'),)
+
+    def __str__(self):
+        des = 'Ingredient {0} from {1}'.format(self.ingredient.name, self.dish.name)
+        return des
+
+
+class Fridge(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default = True)
+
+    def __str__(self):
+        if not self.is_available:
+            insert = 'do not '
+        else:
+            insert = ''
+        description = "Record {0}: User {1} {2}have {3}.".format(self.id, self.user.username, insert, self.ingredient.name)
+        return description
