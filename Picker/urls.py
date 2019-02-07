772b51d4885b010a2c37ee5621db1573cc0867de
Oleksii Bunyk <oleksabunyk@gmail.com>
2019-02-07 11:25:30 +0200

    First inner join in Recipe

--- Picker/urls.py
+++ Picker/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ingredient/<int:ingredient_id>/', views.ing, name='ing'),
+    path('recipe/<int:dish_id>/', views.recipe, name='recipe'),
 ]
--- Picker/views.py
+++ Picker/views.py
@@ -1,5 +1,35 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+from .models import Ingredient, Dish, Recipe
 
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+def ing(request, ingredient_id):
+    try:
+        ingerdient = Ingredient.objects.get(pk=ingredient_id)
+    except Ingredient.DoesNotExist:
+        raise Http404("There is no such ingredient.")
+    context = {
+        'ing_name': ingerdient.name,
+        'ing_description': ingerdient.description,
+    }
+    return render(request, 'ingredient.html', context)
+
+
+def recipe(request, dish_id):
+    try:
+        rec = Dish.objects.get(pk=dish_id)
+    except Dish.DoesNotExist:
+        raise Http404("There is no such recipe.")
+
+    ing_list = Recipe.objects.filter(dish_id = dish_id).select_related('ingredient')
+    context = {
+        'dish_name': rec.name,
+        'ingredients_list': ing_list,
+        'dish_description': rec.description,
+    }
+    return render(request, 'dish.html', context)
