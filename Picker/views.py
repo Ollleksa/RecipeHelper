f19e01e23e4ac81b07657b4ba35e9794b1b2669a
Oleksii Bunyk <oleksabunyk@gmail.com>
2019-02-11 10:51:17 +0200

    lag, bug, but work

--- Picker/views.py
+++ Picker/views.py
@@ -1,22 +1,45 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
+from django.urls import reverse_lazy
 
 from .models import Ingredient, Dish, Recipe, Fridge, recipe_finder
-from .forms import NewIngredient
+from .forms import NewIngredient, NewDish, DishForm, AddIngredient
 
 
 def index(request):
     current_user = request.user
     fridge_ing = Fridge.objects.filter(user_id = current_user.id, is_available = True).select_related('ingredient')
+
+    if request.method == 'POST':
+        for temp in fridge_ing:
+            if str(temp.ingredient_id) in request.POST:
+                temp.is_available = False
+                temp.save()
+
+        form = AddIngredient(request.POST)
+        if form.is_valid():
+            ing_id = form.cleaned_data['ingredient']
+            try:
+                k = Fridge.objects.get(user_id = current_user.id, ingredient_id = ing_id)
+                k.is_available = True
+                k.save()
+            except Fridge.DoesNotExist:
+                k = Fridge(user_id = current_user.id, ingredient_id = ing_id)
+                k.save()
+    else:
+        form = AddIngredient()
+
     available_dish_id = recipe_finder(current_user)
     dish_list = [Dish.objects.get(id=i['id']) for i in available_dish_id]
     context = {
         'user': current_user,
         'ing_list': fridge_ing,
         'dish_list': dish_list,
+        'form': form,
     }
-    return render(request, 'home.html', context)
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render(context, request))
 
 
 def ing(request, ingredient_id):
@@ -37,13 +60,24 @@ def recipe(request, dish_id):
     except Dish.DoesNotExist:
         raise Http404("There is no such recipe.")
 
+    if request.method == 'POST':
+        form = DishForm(request.POST)
+        if form.is_valid():
+            ing2rec = Recipe(dish_id = dish_id, ingredient_id = form.cleaned_data['ingredient'],
+                             amount=form.cleaned_data['amount'])
+            ing2rec.save()
+    else:
+        form = DishForm()
+
     ing_list = Recipe.objects.filter(dish_id = dish_id).select_related('ingredient')
+    template = loader.get_template('dish.html')
     context = {
         'dish_name': rec.name,
         'ingredients_list': ing_list,
         'dish_description': rec.description,
+        'form': form,
     }
-    return render(request, 'dish.html', context)
+    return HttpResponse(template.render(context, request))
 
 
 def catalog_ingredient(request):
@@ -65,8 +99,19 @@ def catalog_ingredient(request):
 
 
 def catalog_recipe(request):
+    if request.method == 'POST':
+        form = NewDish(request.POST)
+        if form.is_valid():
+            d = Dish(name = form.cleaned_data['name'], description = form.cleaned_data['description'])
+            d.save()
+            return HttpResponseRedirect('{}'.format(d.id))
+    else:
+        form = NewDish()
+
     dish_list = Dish.objects.all()
+    template = loader.get_template('catalog.html')
     context = {
         'dish_list': dish_list,
+        'form': form,
     }
-    return render(request, 'catalog.html', context)
+    return HttpResponse(template.render(context, request))
